69143a249dc99de90f01d297116c84afeaf86918
Rafayet Hossen <hossenmdrafayet@gmail.com>
2025-08-13 00:55:53 +0600

    Edit app title and app icon

--- src/main/java/org/example/assignment01shapedrawer/HelloApplication.java
+++ src/main/java/org/example/assignment01shapedrawer/HelloApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.css.Stylesheet;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,7 +14,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
+        stage.setTitle("Shape Drawer");
+        stage.getIcons().add(new Image(HelloApplication.class.getResourceAsStream("shapes.png")));
         stage.setScene(scene);
         stage.show();
     }
