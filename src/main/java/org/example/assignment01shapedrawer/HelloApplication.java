47ae8a165d7ff18a4ddef1e60127890580b8bc8d
Rafayet Hossen <hossenmdrafayet@gmail.com>
2025-08-13 01:37:53 +0600

    clone and update from another devices

--- src/main/java/org/example/assignment01shapedrawer/HelloApplication.java
+++ src/main/java/org/example/assignment01shapedrawer/HelloApplication.java
@@ -1,7 +1,6 @@
 package org.example.assignment01shapedrawer;
 
 import javafx.application.Application;
-import javafx.css.Stylesheet;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
--- src/main/java/org/example/assignment01shapedrawer/HelloController.java
+++ src/main/java/org/example/assignment01shapedrawer/HelloController.java
@@ -2,7 +2,6 @@ package org.example.assignment01shapedrawer;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
